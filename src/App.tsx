d8a9dba2d8a638ba8594cc3a57bfd8e02273c0cb
himanshukk81 <himanshukk81@gmail.com>
2022-01-28 12:55:07 +0530

    include bootstrap file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { UserList } from './components/UserList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
